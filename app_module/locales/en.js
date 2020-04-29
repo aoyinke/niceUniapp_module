a60ac45342dc20ccb95d7809abe40e3bbe9cf0c0
= <=>
2020-04-29 08:10:20 +0800

    4/29

---
+++ app_module/locales/en.js
@@ -0,0 +1,100 @@
+// 英文
+export default {
+  name: "Asako UED",
+  brand: "PhotoArtLife",
+  author: {
+    star: "LEO",
+    position: "Beijing Chaoyang",
+    founder:
+      "A Creator，PhotoArtLife、Asako Studio、MUX Lab Founder | Independent Designer、Photographer、Producer、Software Engineer、Full Stack Developer、Media Author etc."
+  },
+  introduce:
+    "Our team comes from Google、Tencent、Alibaba、Baidu、Xiaomi、Yidao、Blockchain and other Internet software and service industries.\nAnd We dedicated to providing creators with small、beautiful、aggregated creation and sharing content brand marketing services. ",
+  services:
+    "Not limited to the following businesses：\nGraphic design、advertising services、image shooting and production、organizing artistic activities、computer software services and design research and development、brand marketing and planning、digital currency trading and service providers、community operations and media cooperation, etc.",
+  abstract:
+    "A set of highly reusable product interaction and front-end design solutions derived from user and corporate values, and providing progressive experience and visual specifications.",
+  copyright: "@Asako Studio ✘ PhotoArtLife ✘ MUX Lab",
+  labels: [
+    "creative",
+    "design",
+    "photography",
+    "animation",
+    "travel",
+    "creation",
+    "writer",
+    "media",
+    "explore",
+    "program",
+    "code",
+    "product",
+    "producer",
+    "director",
+    "blogger",
+    "record",
+    "vlog",
+    "video",
+    "photo",
+    "reporter",
+    "engineer",
+    "challenge",
+    "imaginative",
+    "laboratory",
+    "studio",
+    "entrepreneurship",
+    "market",
+    "business",
+    "commerce",
+    "blockchain",
+    "bitcoin",
+    "arts",
+    "vision",
+    "adware",
+    "freedom",
+    "theme",
+    "colorful"
+  ],
+  tabs: ["Home", "Extend", "About"],
+  team: [
+    {
+      name: "跨世_PhotoArtLife",
+      avatar:
+        "https://tvax3.sinaimg.cn/crop.4.0.1116.1116.180/967d9727ly8fftw9b3jmmj20v90v0wgq.jpg",
+      tipsImg:
+        "https://mmbiz.qpic.cn/mmbiz_jpg/g7N4GSDkLL6P7tL0Rtm5ExNnt3a6kHMS7tdrsvH8Vv8rUMpwOa0o2W9ichQW2mq84ib9oic1EnxUkPxrNj1f013KQ/0?wx_fmt=jpeg",
+      role: "Founder、Creator、Photographer"
+    },
+    {
+      name: "James",
+      avatar:
+        "http://wx.qlogo.cn/mmopen/lx6nJKHrPibX4TYXXrvNMVoeBf923oQx09OeCP8X7rSajibKtkuyJcticDhSQNhgvqKx46ngn9YHiciaPKLRJYQQY5kV7DRor4OYJ/0",
+      tipsImg:
+        "https://mmbiz.qpic.cn/mmbiz_jpg/g7N4GSDkLL6P7tL0Rtm5ExNnt3a6kHMSgog8hH6wibFpXG4oJLKib9V623yPARIpDrR9uwHZabSTHoGZOvHT9rRw/0?wx_fmt=jpeg",
+      role: "Market Director、Hacker"
+    },
+    {
+      name: "Hepburn",
+      avatar:
+        "https://tvax4.sinaimg.cn/crop.0.0.1002.1002.180/007q1594ly8garlm6zz9aj30ru0rutc2.jpg",
+      role: "Engineer、Outdoor Explorer",
+      target: "https://mp.weixin.qq.com/s/sy8iYBn_Yu95UUzyUyzTRg"
+    },
+    {
+      name: "Asako Studio",
+      avatar:
+        "https://lz.sinaimg.cn/orj1080/967d9727ly3gc0whyiy96j20sg0sg1jj.jpg",
+      role: "Designer、Visual Artist",
+      target: "https://mp.weixin.qq.com/s/sy8iYBn_Yu95UUzyUyzTRg"
+    },
+    {
+      name: "MUX lab",
+      avatar:
+        "https://6173-asako-studio-9c2pl-1301657606.tcb.qcloud.la/static/face_id2_effect_by_gleb.gif?sign=a5612a42fe35267069bdbf330d4070ab&t=1587180383",
+      role: "Mystery Product Owner",
+      target: "https://mp.weixin.qq.com/s/TeRiItQrpMY8dooQopDN3A"
+    }
+  ],
+  footer: {
+    buttons: ["Share", "Message"]
+  }
+};
